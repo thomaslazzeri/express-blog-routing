9ea3674e0b18711ca70f15a94ae471ffc3d9f952
thomas <thomaslazzeri3@gmail.com>
2026-09-02 19:43:45 +0200

    Add initial posts data structure with sample posts

---
+++ reference/posts.js
@@ -0,0 +1,17 @@
+const posts = [
+    {
+        id: 1, 
+        title: 'Primo post',
+        content: 'Contenuto del primo post'
+    },
+    {
+        id: 2 , 
+        title: 'Secondo post',
+        content: 'Contenuto del secondo post'
+    },
+    {
+        id: 3 , 
+        title: 'Terzo post',
+        content: 'Contenuto del terzo post'
+    }
+];
