d82b1f0bcd4faac349cd4e080b6c79550a3cc808
thomas <thomaslazzeri3@gmail.com>
2026-09-02 18:11:01 +0200

    Add GET and DELETE routes for posts in server.js

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
+app.get('/posts', (req, res) => {
+    res.send("Lista dei post");
+});
+
+app.delete('/posts/1', (req, res) => {
+    res.send("Cancellazione del post");
+});
